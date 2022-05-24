042ffc1749b4fd0204cc30bb9ecbef352b83f985
shubham Chavan <shubhamchavan73200@gmail.com>
2022-05-24 18:59:56 +0530

    Validate Date of birth feature is added

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -3,29 +3,35 @@ function verifyPassword() {
     var pass = document.getElementById("password").value;
     var repass = document.getElementById("confirm_password").value;
     //check empty password field
+    var returnval = true;
+
     if(pass == "") {
         alert("Fill the password please!");
         document.getElementById("hiddenSubmit").value="Fill the password please!";
+        returnval = false;
     }
 
     //minimum password length validation
     if(pass.length < 8) {
         alert("Password length must be atleast 8 characters");
         document.getElementById("hiddenSubmit").value="Password length must be atleast 8 characters";
+        returnval = false;
     }
 
     //maximum length of password validation
     if(pass.length > 15) {
         alert("Password length must not exceed 15 characters");
         document.getElementById("hiddenSubmit").value="Password Length gone above 15";
+        returnval = false;
     }
 
     //Check password matching or npt
     if(pass !== repass){
         alert("Password Doesn't match");
         document.getElementById("hiddenSubmit").value="Password Doesn't match";
+        returnval = false;
     }
-
+    return returnval;
 }
 
 //------------------------------------------Set Present Address------------------------------------------
@@ -49,3 +55,42 @@ var SameAsAbove = {
 }
 
 Core.start(SameAsAbove);
+
+//----------------------------------- Verify Age -----------------------------------
+function dobValidate(birth) {
+
+
+    var today = new Date();
+    var nowyear = today.getFullYear();
+    var nowmonth = today.getMonth();
+    var nowday = today.getDate();
+    var b = document.getElementById('dob').value;
+
+
+
+    var birth = new Date(b);
+
+    var birthyear = birth.getFullYear();
+    var birthmonth = birth.getMonth();
+    var birthday = birth.getDate();
+
+    var age = nowyear - birthyear;
+    var age_month = nowmonth - birthmonth;
+    var age_day = nowday - birthday;
+
+
+    var returval = true;
+    if (age > 100) {
+        alert("Age cannot be more than 100 Years.Please enter correct age");
+        returval = false;
+    }
+    if (age_month < 0 || (age_month == 0 && age_day < 0)) {
+        age = parseInt(age) - 1;
+    }
+    if ((age == 18 && age_month <= 0 && age_day <= 0) || age < 18) {
+        alert("Age should be more than 18 years.Please enter a valid Date of Birth");
+        returval = false;
+    }
+
+    return returval;
+}
